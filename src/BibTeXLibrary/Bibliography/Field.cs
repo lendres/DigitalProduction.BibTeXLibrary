34542466068228583042d6d5334229055098dca9
Lance A. Endres <lanceendres@gmail.com>
2026-05-06 05:18:13 -0500

    Create Field.cs

---
+++ src/BibTeXLibrary/Bibliography/Field.cs
@@ -0,0 +1,55 @@
+﻿using DigitalProduction.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibTeXLibrary;
+
+public class Field : NotifyPropertyModifiedChanged
+{
+	#region Construction
+
+	#endregion
+
+	#region Properties
+
+	/// <summary>
+	/// Name of the string constant.
+	/// </summary>
+	public string Name
+	{
+		get => GetValueOrDefault<string>(string.Empty);
+		set => SetValue(value);
+	}
+
+	/// <summary>
+	/// Value of the string constant.
+	/// </summary>
+	public string Value
+	{
+		get => TagValue.Content;
+
+		set
+		{
+			if (TagValue.Content != value)
+			{
+				TagValue.Content = value;
+				OnPropertyChanged();
+				OnPropertyChanged(nameof(TagValue));
+			}
+		}
+	}
+
+	/// <summary>
+	/// Value of the string constant.
+	/// </summary>
+	public FieldValue TagValue
+	{
+		get => GetValueOrDefault<FieldValue>(new FieldValue(string.Empty, FieldValueType.String));
+		protected set => SetValue(value);
+	}
+
+	#endregion
+}

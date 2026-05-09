1da250f30c28757d24bf8b27dcc74771daf467c9
Lance A. Endres <lanceendres@gmail.com>
2026-05-09 16:56:03 -0500

    Create BibliographyWriteException .cs

---
+++ src/BibTeXLibrary/Exceptions/BibliographyWriteException .cs
@@ -0,0 +1,37 @@
+﻿namespace BibTeXLibrary;
+
+/// <summary>
+/// Exception thrown when bibliography output cannot be written correctly.
+/// </summary>
+public class BibliographyWriteException : Exception
+{
+	#region Construction
+
+	/// <summary>
+	/// Default constructor.
+	/// </summary>
+	public BibliographyWriteException()
+	{
+	}
+
+	/// <summary>
+	/// Message constructor.
+	/// </summary>
+	/// <param name="message">The exception message.</param>
+	public BibliographyWriteException(string message)
+		: base(message)
+	{
+	}
+
+	/// <summary>
+	/// Message and inner exception constructor.
+	/// </summary>
+	/// <param name="message">The exception message.</param>
+	/// <param name="innerException">The inner exception.</param>
+	public BibliographyWriteException(string message, Exception innerException)
+		: base(message, innerException)
+	{
+	}
+
+	#endregion
+}
